803fcf9ff079e67023f4d3bc890aacc50b204783
Navneeth <vrnavaneeth135@gmail.com>
2022-12-19 21:56:36 +0530

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your code here
+ //your code here
+let input = document.getElementById("input");
+
+function display(num) {
+    input.value += num
+}
+
+function calculate() {
+    try{
+        input.value = eval(input.value);
+    }
+
+    catch(err) {
+        input.value = "Invalid";
+    }
+}
+
+function clr() {
+    input.value = "";
+}
+
+function del() {
+    if(input.value == "NaN" || input.value=="Infinity"){
+        clr()
+    }
+    input.value = input.value.slice(0,-1)
+}
